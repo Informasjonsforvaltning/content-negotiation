0334085e1676fbe9f568f50be3244cd39deb3299
Stig B. Dørmænen <stigbd@gmail.com>
2022-09-05 10:34:20 +0200

    chore: refactor signature

--- src/content_negotiation/content_negotiation.py
+++ src/content_negotiation/content_negotiation.py
@@ -8,6 +8,7 @@ Example:
     >>> decide_content_type(accept, supported)
     'application/json'
 """
+from enum import Enum
 import logging
 from typing import Any, List, Optional
 
@@ -18,12 +19,21 @@ class InvalidMediaRangeError(ValueError):
     pass
 
 
+class MediaRangeSpecificity(Enum):
+    """Enum for media range specificity."""
+
+    NONSPECIFIC = 0
+    SUBTYPE_INSPECIFIC = 1
+    SPECIFIC = 2
+
+
 class WeightedMediaRange:
     """Class for handling weighted media ranges."""
 
     type: str
     sub_type: str
     q: float = 1.0
+    specificity: MediaRangeSpecificity
 
     def __init__(self, media_range: str) -> None:
         """Initialize the weighted media range."""
@@ -34,6 +44,14 @@ class WeightedMediaRange:
                 f"Assigning q-parameter for weighted media range: {media_range}"
             )
             self.type, self.sub_type = weighted_media_range_split[0].split("/")
+            # Check if media range is specific:
+            if self.type == "*":
+                self.specificity = MediaRangeSpecificity.NONSPECIFIC
+            elif self.sub_type == "*":
+                self.specificity = MediaRangeSpecificity.SUBTYPE_INSPECIFIC
+            else:
+                self.specificity = MediaRangeSpecificity.SPECIFIC
+
             # If q-parameter is present, assign it:
             for weighted_media_range_part in weighted_media_range_split[1:]:
                 if weighted_media_range_part.startswith("q="):
@@ -54,6 +72,16 @@ class WeightedMediaRange:
             return self.type == other.type and self.sub_type == other.sub_type
         return False
 
+    def __lt__(self, other: Any) -> bool:  # pragma: no cover
+        """Compare two weighted media ranges."""
+        if isinstance(other, WeightedMediaRange):
+            # If weighted media ranges are equal, compare specificity:
+            if self.q == other.q:
+                return self.specificity.value < other.specificity.value
+            # If weighted media ranges are not equal, compare q value:
+            return self.q < other.q
+        return False
+
     def __str__(self) -> str:
         """Return the weighted media range as a string."""
         return f"{self.type}/{self.sub_type};q={self.q}"
@@ -64,46 +92,31 @@ class WeightedMediaRange:
 
 
 def prepare_weighted_media_ranges(
-    accept_weighted_media_ranges: List[str],
+    weighted_media_ranges: List[str],
 ) -> List[WeightedMediaRange]:
     """Prepare the accept weighted media ranges and sort on q-parameter."""
-    logging.debug(
-        f"Preparing accept weighted media ranges: {accept_weighted_media_ranges}"
-    )
+    logging.debug(f"Preparing accept weighted media ranges: {weighted_media_ranges}")
     # Assign q-parameter:
-    accept_weighted_media_ranges_sorted: List[WeightedMediaRange] = []
+    weighted_media_ranges_sorted: List[WeightedMediaRange] = []
 
-    for accept_weighted_media_range in accept_weighted_media_ranges:
+    for accept_weighted_media_range in weighted_media_ranges:
         # Instantiate weighted media range:
         try:
             weighted_media_range = WeightedMediaRange(accept_weighted_media_range)
 
-            accept_weighted_media_ranges_sorted.append(weighted_media_range)
+            weighted_media_ranges_sorted.append(weighted_media_range)
         except InvalidMediaRangeError:
             logging.debug(
                 "Ignoring invalid weighted media range: %s", accept_weighted_media_range
             )
             pass  # ignore invalid media range
 
-    # Adjust q-parameters with regard to specificity:
-    # Highest q-parameter is the most specific:
-    for weighted_media_range in accept_weighted_media_ranges_sorted:
-        logging.debug(
-            f"Ajusting q-parameter for weighted media range: {weighted_media_range}"
-        )
-        if weighted_media_range.type == "*":
-            pass
-        elif weighted_media_range.sub_type == "*":
-            weighted_media_range.q = weighted_media_range.q + 0.0001
-        else:
-            weighted_media_range.q = weighted_media_range.q + 0.0002
-
-    # Sort on q-parameter and return list of weighted media ranges:
-    accept_weighted_media_ranges_sorted.sort(key=lambda x: x.q, reverse=True)
+    # Sort and return list of weighted media ranges:
+    weighted_media_ranges_sorted.sort(reverse=True)
     logging.debug(
-        f"Accept weighted media ranges sorted: {', '.join(str(p) for p in accept_weighted_media_ranges_sorted)}"  # noqa: B950
+        f"Accept weighted media ranges sorted: {', '.join(str(p) for p in weighted_media_ranges_sorted)}"  # noqa: B950
     )
-    return accept_weighted_media_ranges_sorted
+    return weighted_media_ranges_sorted
 
 
 def get_default_content_type(
@@ -131,12 +144,12 @@ def is_media_range_type_in_supported_content_types(
 
 
 def decide_content_type(
-    accept_weighted_media_ranges: List[str], supported_content_types: List[str]
+    accept_headers: List[str], supported_content_types: List[str]
 ) -> Optional[str]:
-    """Decide the content type of the response.
+    """Decide the content type based on the given accept-header and supported content-types.
 
     Args:
-        accept_weighted_media_ranges (List[str]): List of weighted media ranges from the accept header. # noqa: B950
+        accept_headers (List[str]): the accept headers.
         supported_content_types (List[str]): List of supported content types.
 
     Returns:
@@ -144,18 +157,19 @@ def decide_content_type(
 
     """
     logging.debug(
-        f"Deciding content types {accept_weighted_media_ranges} "
-        f"against {supported_content_types}"
+        f"Deciding content types {accept_headers} " f"against {supported_content_types}"
     )
     # Checking a couple of corner cases:
-    if len(supported_content_types) == 0 or len(accept_weighted_media_ranges) == 0:
+    if len(supported_content_types) == 0 or len(accept_headers) == 0:
         return None
 
-    content_type: Optional[str] = None
-    accept_weighted_media_ranges_sorted = prepare_weighted_media_ranges(
-        accept_weighted_media_ranges
+    weighted_media_ranges: List[str] = (
+        ",".join(accept_headers).replace(" ", "").split(",")
     )
-    for weighted_media_range in accept_weighted_media_ranges_sorted:
+
+    content_type: Optional[str] = None
+    weighted_media_ranges_sorted = prepare_weighted_media_ranges(weighted_media_ranges)
+    for weighted_media_range in weighted_media_ranges_sorted:
         logging.debug(f"Checking weighted media range: {weighted_media_range}")
         if weighted_media_range in supported_content_types:
             content_type = weighted_media_range.media_range()
--- tests/test_content_negotiation.py
+++ tests/test_content_negotiation.py
@@ -14,179 +14,149 @@ SUPPORTED_CONTENT_TYPES = [
 
 def test_content_negotiation() -> None:
     """Should return 200 and correct content-type."""
-    accept_weighted_media_ranges: List[str] = ["text/turtle", "application/ld+json"]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
+    accept_header: List[str] = ["text/turtle", "application/ld+json"]
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
     assert (
         "text/turtle" == content_type
-    ), f"For header-value '{accept_weighted_media_ranges}', content-type should be text/turtle."  # noqa: B950
+    ), f"For header-value '{accept_header}', content-type should be text/turtle."  # noqa: B950
 
 
 def test_content_negotiation_2() -> None:
     """Should return 200 and correct content-type."""
-    accept_weighted_media_ranges: List[str] = ["application/ld+json", "text/turtle"]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
+    accept_header: List[str] = ["application/ld+json", "text/turtle"]
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
     assert (
         "application/ld+json" == content_type
-    ), f"For '{accept_weighted_media_ranges}', content-type should be application/ld+json."  # noqa: B950
+    ), f"For '{accept_header}', content-type should be application/ld+json."  # noqa: B950
 
 
 def test_content_negotiation_3() -> None:
     """Should return 200 and correct content-type."""
-    accept_weighted_media_ranges: List[str] = ["not/acceptable", "*/*"]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
+    accept_header: List[str] = ["not/acceptable", "*/*"]
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
     assert (
         "text/turtle" == content_type
-    ), f"For header-value '{accept_weighted_media_ranges}', content-type should be text/turtle."  # noqa: B950
+    ), f"For header-value '{accept_header}', content-type should be text/turtle."  # noqa: B950
 
 
 def test_content_negotiation_4() -> None:
     """Should return 200 and correct content-type."""
-    accept_weighted_media_ranges: List[str] = ["text/plain", "*/*"]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
+    accept_header: List[str] = ["text/plain", "*/*"]
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
     assert (
         "text/turtle" == content_type
-    ), f"For header-value '{accept_weighted_media_ranges}', content-type should be text/turtle."  # noqa: B950
+    ), f"For header-value '{accept_header}', content-type should be text/turtle."  # noqa: B950
 
 
 def test_content_negotiation_5() -> None:
     """Should return 200 and correct content-type."""
-    accept_weighted_media_ranges: List[str] = ["*/*", "text/plain"]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
+    accept_header: List[str] = ["*/*", "text/plain"]
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
     assert (
         "text/turtle" == content_type
-    ), f"For header-value '{accept_weighted_media_ranges}', content-type should be text/turtle."  # noqa: B950
+    ), f"For header-value '{accept_header}', content-type should be text/turtle."  # noqa: B950
 
 
 def test_content_negotiation_6() -> None:
     """Should return 200 and correct content-type."""
-    accept_weighted_media_ranges: List[str] = [
+    accept_header: List[str] = [
         "application/json",
         "application/*",
         "*/*",
     ]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
     assert (
         "application/rdf+xml" == content_type
-    ), f"For header-value '{accept_weighted_media_ranges}', content-type should be application/rdf+xml."  # noqa: B950
+    ), f"For header-value '{accept_header}', content-type should be application/rdf+xml."  # noqa: B950
 
 
 def test_content_negotiation_7() -> None:
     """Should return 200 and correct content-type."""
-    accept_weighted_media_ranges: List[str] = [
+    accept_header: List[str] = [
         "*/*;q=0.8",
         "text/plain",
         "application/signed-exchange;q=0.9",
         "application/ld+json",
     ]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
     assert (
         "application/ld+json" == content_type
-    ), f"For header-value '{accept_weighted_media_ranges}', content-type should be application/ld+json."  # noqa: B950
+    ), f"For header-value '{accept_header}', content-type should be application/ld+json."  # noqa: B950
 
 
 def test_content_negotiation_8() -> None:
     """Should return 200 and correct content-type."""
-    accept_weighted_media_ranges: List[str] = [
+    accept_header: List[str] = [
         "*/*;q=0.8;v=b3",
         "text/plain",
         "application/signed-exchange;v=b3;q=0.9",
         "application/ld+json",
     ]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
     assert (
         "application/ld+json" == content_type
-    ), f"For header-value '{accept_weighted_media_ranges}', content-type should be application/ld+json."  # noqa: B950
+    ), f"For header-value '{accept_header}', content-type should be application/ld+json."  # noqa: B950
 
 
 def test_content_negotiation_9() -> None:
     """Should return 200 and correct content-type."""
-    accept_weighted_media_ranges: List[str] = ["application/ld+json;v=b3", "*/*"]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
+    accept_header: List[str] = ["application/ld+json;v=b3", "*/*"]
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
     assert (
         "application/ld+json" == content_type
-    ), f"For header-value '{accept_weighted_media_ranges}', content-type should be application/ld+json."  # noqa: B950
+    ), f"For header-value '{accept_header}', content-type should be application/ld+json."  # noqa: B950
 
 
 def test_content_negotiation_10() -> None:
     """Should return 200 and correct content-type."""
-    accept_weighted_media_ranges: List[str] = ["*/*", "text/*", "application/ld+json"]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
+    accept_header: List[str] = ["*/*", "text/*", "application/ld+json"]
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
     assert (
         "application/ld+json" == content_type
-    ), f"For header-value '{accept_weighted_media_ranges}', content-type should be application/ld+json."  # noqa: B950
+    ), f"For header-value '{accept_header}', content-type should be application/ld+json."  # noqa: B950
 
 
 def test_content_negotiation_11() -> None:
     """Should return 200 and correct content-type."""
-    accept_weighted_media_ranges: List[str] = ["application/*", "text/turtle;q=0.2"]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
+    accept_header: List[str] = ["application/*", "text/turtle;q=0.2"]
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
     assert (
         "application/rdf+xml" == content_type
-    ), f"For header-value '{accept_weighted_media_ranges}', content-type should be application/rdf+xml."  # noqa: B950
+    ), f"For header-value '{accept_header}', content-type should be application/rdf+xml."  # noqa: B950
 
 
 def test_content_negotiation_12() -> None:
     """Should return None."""
-    accept_weighted_media_ranges: List[str] = ["text/"]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
-    assert content_type is None, f"'{accept_weighted_media_ranges}' failed"
+    accept_header: List[str] = ["text/"]
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
+    assert content_type is None, f"'{accept_header}' failed"
 
 
 def test_content_negotiation_13() -> None:
     """Should return None."""
-    accept_weighted_media_ranges: List[str] = ["text"]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
-    assert content_type is None, f"'{accept_weighted_media_ranges}' failed"
+    accept_header: List[str] = ["text"]
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
+    assert content_type is None, f"'{accept_header}' failed"
 
 
 def test_content_negotiation_14() -> None:
     """Should return None."""
-    accept_weighted_media_ranges: List[str] = ["audio/*"]
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
-    assert content_type is None, f"'{accept_weighted_media_ranges}' failed"
+    accept_header: List[str] = ["audio/*"]
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
+    assert content_type is None, f"'{accept_header}' failed"
 
 
 def test_content_negotiation_no_supported_content_types() -> None:
     """Should return None."""
-    accept_weighted_media_ranges: List[str] = ["*/*"]
-    content_type = decide_content_type(accept_weighted_media_ranges, [])
-    assert content_type is None, f"'{accept_weighted_media_ranges}' failed"
+    accept_header: List[str] = ["*/*"]
+    content_type = decide_content_type(accept_header, [])
+    assert content_type is None, f"'{accept_header}' failed"
 
 
-def test_content_negotiation_no_accept_weighted_media_ranges() -> None:
+def test_content_negotiation_no_accept_header() -> None:
     """Should return default content-type."""
-    accept_weighted_media_ranges: List[str] = []
-    content_type = decide_content_type(
-        accept_weighted_media_ranges, SUPPORTED_CONTENT_TYPES
-    )
+    accept_header: List[str] = []
+    content_type = decide_content_type(accept_header, SUPPORTED_CONTENT_TYPES)
     assert (
         content_type is None
-    ), f"For header-value '{accept_weighted_media_ranges}', content-type should be None."
+    ), f"For header-value '{accept_header}', content-type should be None."
